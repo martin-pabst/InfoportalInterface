2bd8c84ed370baa6234cc18054bdb5389e2e543a
martin-pabst <asv.pabst@gmail.com>
2017-03-20 14:02:49 +0100

    .

--- src/test/java/TestInfoPortalInterfaceFactory.java
+++ src/test/java/TestInfoPortalInterfaceFactory.java
@@ -7,7 +7,7 @@ public class TestInfoPortalInterfaceFactory {
 	public static InfoPortalInterface getInfoPortalInterface() {
 
 		if (ipi == null) {
-			ipi = new InfoPortalInterface("martin.pabst", "Schrobenhausen1mp",
+			ipi = new InfoPortalInterface("", "",
 					"https://portal.mzml.de/portal/csgying/schule_portal/index_dir/index.php");
 		}
 
